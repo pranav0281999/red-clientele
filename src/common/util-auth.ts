2fa51936e85b5c4954b19ade0f7ae13b86150a32
pranav0281999 <pranav0281999@gmail.com>
2022-10-18 23:09:43 +0530

    Updates client id

--- src/common/util-auth.ts
+++ src/common/util-auth.ts
@@ -1,6 +1,6 @@
 export function redirectToOAuth() {
   window.open(
-    "https://www.reddit.com/api/v1/authorize?client_id=2G_1fZBEII27CUGHkg49Ng&response_type=code&state=auth&redirect_uri=http://localhost:3000&duration=permanent&scope=identity,read,vote",
+    "https://www.reddit.com/api/v1/authorize?client_id=ALQwmg16YCEWNOQRJMratw&response_type=code&state=auth&redirect_uri=http://localhost:3000&duration=permanent&scope=identity,read,vote",
     "_self"
   );
 }
